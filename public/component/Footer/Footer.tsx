08a5ee09e488b1b1de230421c38e0fb31c51c430
sunnydcitruspunch <wiwisun18@gmail.com>
2023-01-29 09:35:12 -0700

    changes

---
+++ public/component/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div id='footerposition'>
+      <p id='footer'>© &nbsp;2023 &nbsp;SUNNY YANG</p>
+    </div>
+  )
+}
+
+export default Footer
